40fb6d3d01e994d421598983b38df742b99b99db
‘Katrin <katrin.baier77@gmail.com>
2022-11-09 00:30:15 +0100

    kat components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Hero from './components/Hero.jsx';
+import ModalInstructions from './components/ModalInstructions.jsx';
 import Layout from './components/Layout.jsx';
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Hero />}></Route>
+
         </Route>
       </Routes>
     </BrowserRouter >
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,9 @@
 import { useContext } from 'react';
 import { Context } from '../ConnectDB.jsx';
+import './Hero.css';
+import Instructions from './Instructions.jsx';
+import PerformanceBars from './PerformanceBars.jsx';
+
 
 
 const Hero = () => {
@@ -7,7 +11,14 @@ const Hero = () => {
 
 
     return (
-        <div></div>
+        <>
+            <PerformanceBars />
+            <div className='flex justify-center'>
+                <button className="bg-sky-600 px-6 py-3 rounded-full cursor-pointer text-white absolute bottom-8 z-20">Choose Pokemon</button>
+                <div className='wrestle-area absolute bottom-0 z-10'></div>
+            </div>
+            <Instructions />
+        </>
     )
 }
 
