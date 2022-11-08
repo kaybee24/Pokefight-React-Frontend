299e41601f1dddeca5eba5528d4a9efd9df2868a
‘Katrin <katrin.baier77@gmail.com>
2022-11-08 09:25:08 +0100

    add navbar slider two

--- src/ConnectDB.jsx
+++ src/ConnectDB.jsx
@@ -1,31 +1,39 @@
 import { useState, useEffect, createContext } from "react";
-import axios from "axios";
 
-export const ConnectDB = createContext();
+export const Context = createContext();
+
 export const ContextProvider = (props) => {
     const [pokemonData, setPokemonData] = useState([]);
     const [pokemonImages, setPokemonImages] = useState([]);
 
     useEffect(() => {
-        const getPokeData = axios.get(import.meta.env.VITE_HEROKU_API)
-        const getPokeImages = axios.get(import.meta.env.VITE_POKE_API)
-            .then(axios.spread((...allData) => {
-                setPokemonData(allData[0].data)
-                setPokemonImages(allData[1].data)
-            }))
-            .catch(err => err.message)
+        fetch(import.meta.env.VITE_HEROKU_API)
+            .then(res => res.json())
+            .then((res) => {
+                setPokemonData(res)
+            })
+            .catch(err => {
+                console.log("Error", err);
+            });
+        fetch(import.meta.env.VITE_POKE_API)
+            .then(res => res.json())
+            .then(res => setPokemonImages(res.results))
+            .catch(err => {
+                console.log("Error", err);
+            });
     }, []);
-
-    return
-    <Context.Provider value={
-        {
-            pokemonData, setPokemonData,
-            pokemonImages, setPokemonImages
-        }
-    }>
-        {props.children}
-    </Context.Provider>
-
+    return (
+        <Context.Provider value={
+            {
+                pokemonData, setPokemonData,
+                pokemonImages, setPokemonImages
+            }
+        }>
+            {props.children}
+        </Context.Provider>
+    )
 }
 
+
 export default ContextProvider;
+
