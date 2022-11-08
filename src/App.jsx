cdb1cf58ffe7784372dc6415a4c7e450278098ea
‘Katrin <katrin.baier77@gmail.com>
2022-11-08 09:15:36 +0100

    add navbar slider daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero.jsx';
+import Layout from './components/Layout.jsx';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Hero />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Hero />}></Route>
+      </Route>
     </Routes>
   )
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useContext, useState } from 'react';
+import { Context } from '../ConnectDB';
+
 
 const Slider = () => {
+    const { pokemonData, pokemonImages } = useContext(Context);
     return (
-        <div>Slider</div>
+        <div className="carousel w-full">
+            <>
+                {pokemonData.map((e) => pokemonData.name && pokemonData.name
+                    (<div id="slide1" className="carousel-item relative w-full" >
+                        <img src="https://placeimg.com/800/200/arch" className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide4" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>)
+
+                )
+                }
+            </>
+        </div >
     )
 }
 
