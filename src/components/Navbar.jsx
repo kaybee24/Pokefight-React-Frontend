3c95399aaaa8aa2f5864d9fb1aaf2b89b4504507
samin <fatehsamin@gmail.com>
2022-11-10 15:17:04 +0100

    search2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
-                    <input type="text" placeholder="Search2" className="input input-bordered" />
+                    <input type="text" placeholder="Search" className="input input-bordered" />
                 </div>
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
