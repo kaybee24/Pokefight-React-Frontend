7647879ec6f876a0ad1d130b431e7c8edbc75a57
‘Katrin <katrin.baier77@gmail.com>
2022-11-10 15:01:52 +0100

    update modal

--- src/components/ModalInstructions.jsx
+++ src/components/ModalInstructions.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 
-const ModalInstructions = ({ open, onClose }) => {
-    if (!open) return null
-
+const ModalInstructions = () => {
+    /*     if (!open) return null
+     */
     return (
         <>
-            <div className='fixed w-full h-full z-50 pt-10 bg-black/50'>
-                <div className='max-w-[25rem] h-[30rem] rounded-xl drop-shadow-lg mx-auto bg-slate-300 text-slate-900'>
-                    <div className='px-4 text-center'>
-                        <p onClick={onClose} className="text-right font-bold cursor-pointer">X</p>
-                        <h1 className='font-bold text-4xl uppercase pb-6'>Instructions</h1>
-                        <p className='pb-5 px-6'>Select a character</p>
-                        <p className='pb-5 px-6'>Computer chooses a random character</p>
-                        <p className='pb-5 px-6'>Select the performance category next to your pokemon</p>
-                        <button onClick={onClose} className="bg-sky-600 px-6 py-3 mb-4 rounded-full cursor-pointer text-white">Choose Pokemon</button>
-                    </div>
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative bg-slate-300 text-center">
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h2 className="text-sky-600 pb-4 text-4xl uppercase font-bold">Instructions</h2>
+                    <p className="py-4">Select a character</p>
+                    <p className="py-4">Computer chooses a random character</p>
+                    <p className="py-4">Select the performance category next to your pokemon</p>
+                    <p className="py-4">Choose a theme</p>
                 </div>
             </div>
         </>
@@ -22,3 +21,4 @@ const ModalInstructions = ({ open, onClose }) => {
 }
 
 export default ModalInstructions
+
